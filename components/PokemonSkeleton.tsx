1b5281654416ac6c1e28ace4ceccdae42be1f428
carlp <official.carlpahuyo@gmail.com>
2022-06-03 21:24:21 +0800

    change skeleton mini item size

--- components/PokemonSkeleton.tsx
+++ components/PokemonSkeleton.tsx
@@ -4,7 +4,7 @@ interface PokemonSkeletonProps {
 
 const PokemonSkeleton = ({ color }: PokemonSkeletonProps) => {
   return (
-    <div className={['h-36 md:h-48 p-4 rounded-3xl w-full', color].join(' ')}>
+    <div className={['h-40 md:h-48 p-4 rounded-3xl w-full', color].join(' ')}>
       <div className="animate-pulse flex items-start gap-4 md:gap-14">
         <div className="rounded-3xl bg-white/50 w-44 md:w-48 h-28 md:h-36" />
         <div className="w-full flex flex-col gap-2">
@@ -14,7 +14,7 @@ const PokemonSkeleton = ({ color }: PokemonSkeletonProps) => {
             <div className="rounded-3xl bg-white/50 w-12 md:w-14 h-5 md:h-6" />
             <div className="rounded-3xl bg-white/50 w-12 md:w-14 h-5 md:h-6" />
           </div>
-          <div className="rounded-lg bg-white/50 w-28 h-10" />
+          <div className="rounded-lg bg-white/50 w-28 h-16 md:h-12" />
         </div>
       </div>
     </div>
